e956d3fc54b98284cd0b955313c21170c6ad974c
Ross Hale <dev@lottay.com>
2010-09-08 19:27:28 -0700

    rh/jw/jl/jm - first commit of our sweet rails 3 montage project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Montage::Application.config.secret_token = 'b711c058326f24ca9578e8fabd31fae28908d1ce45b39d624094575153ad43ae5b27d1797f58fb6535d8fdfe86200b79394c8caa53e8951be85aaf1529adcb06'
